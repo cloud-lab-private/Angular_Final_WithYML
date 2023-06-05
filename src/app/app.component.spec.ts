188ff616a4956b03a5deeb3f74affa26f529a1b3
Trainer <126759402+Test-Revature@users.noreply.github.com>
2023-06-05 13:01:12 +0530

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,11 +15,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'binding'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('binding');
-  });
-
 });
